4dc2b290e4080a7084421fecc8f776d58b2d5937
shibayan <me@shibayan.jp>
2017-01-08 23:04:45 +0900

    split deploy script

---
+++ create_custom_ami.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+instanceId = $(aws ec2 run-instances --cli-input-json file://custom-ami.json --user-data file://userdata.txt | jq -r '.Instances[0].InstanceId')
+
+aws ec2 wait instance-stopped --instance-ids ${instanceId}
+
+aws ec2 create-image --instance-id ${instanceId} --name custom-ami
