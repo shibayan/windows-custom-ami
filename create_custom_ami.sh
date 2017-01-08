b41f8e477f3030f138fb0708cd6e224601f62471
shibayan <me@shibayan.jp>
2017-01-08 23:11:07 +0900

    fix waiter

--- create_custom_ami.sh
+++ create_custom_ami.sh
@@ -2,6 +2,7 @@
 
 instanceId=$(aws ec2 run-instances --cli-input-json file://custom-ami.json --user-data file://userdata.txt | jq -r '.Instances[0].InstanceId')
 
+aws ec2 wait instance-status-ok --instance-ids ${instanceId}
 aws ec2 wait instance-stopped --instance-ids ${instanceId}
 
 aws ec2 create-image --instance-id ${instanceId} --name custom-ami
