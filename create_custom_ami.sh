b3b46879308716a5740371dfa349375819e0d396
shibayan <me@shibayan.jp>
2017-01-09 13:28:18 +0900

    change base ami

--- create_custom_ami.sh
+++ create_custom_ami.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+echo "ec2 run-instances"
 instanceId=$(aws ec2 run-instances --cli-input-json file://custom-ami.json --user-data file://userdata.txt | jq -r '.Instances[0].InstanceId')
 
+echo "wait instance-status-ok - ${instanceId}"
 aws ec2 wait instance-status-ok --instance-ids ${instanceId}
+
+echo "wait instance-stopped - ${instanceId}"
 aws ec2 wait instance-stopped --instance-ids ${instanceId}
 
+echo "ec2 create-image"
 imageId=$(aws ec2 create-image --instance-id ${instanceId} --name windows-ami-$(date "+%Y%m%d-%H%M%S") | jq -r '.ImageId')
 
+echo "wait image-available - ${imageId}"
 aws ec2 wait image-available --image-ids ${imageId}
 
+echo "ec2 terminate-instances - ${instanceId}"
 aws ec2 terminate-instances --instance-ids ${instanceId}
