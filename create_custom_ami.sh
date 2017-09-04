77e16c1b1e6d977a713c0c2ccf15952d562cba98
shibayan <me@shibayan.jp>
2017-09-04 16:49:05 +0900

    fix output

--- create_custom_ami.sh
+++ create_custom_ami.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "ec2 run-instances"
-instanceId=$(aws ec2 run-instances --cli-input-json file://custom-ami.json --user-data file://userdata.txt --query "Instances[0].InstanceId")
+instanceId=$(aws ec2 run-instances --cli-input-json file://custom-ami.json --user-data file://userdata.txt --query "Instances[0].InstanceId" --output text)
 
 echo "wait instance-status-ok - ${instanceId}"
 aws ec2 wait instance-status-ok --instance-ids ${instanceId}
@@ -10,7 +10,7 @@ echo "wait instance-stopped - ${instanceId}"
 aws ec2 wait instance-stopped --instance-ids ${instanceId}
 
 echo "ec2 create-image"
-imageId=$(aws ec2 create-image --instance-id ${instanceId} --name windows-ami-$(date "+%Y%m%d-%H%M%S") --query "ImageId")
+imageId=$(aws ec2 create-image --instance-id ${instanceId} --name windows-ami-$(date "+%Y%m%d-%H%M%S") --query "ImageId" --output text)
 
 echo "wait image-available - ${imageId}"
 aws ec2 wait image-available --image-ids ${imageId}
